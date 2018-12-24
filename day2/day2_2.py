657496b9ec868b791fd4a9284de9fe2480371388
travipross <travisprosser@gmail.com>
2018-12-24 09:07:32 -0400

    fix printing

--- day2/day2_2.py
+++ day2/day2_2.py
@@ -26,12 +26,14 @@ for idx in range(len(data)):
 def remove_idx_from_string(s, idx):
     return s[:idx] + s[idx+1:]
 
-s = a1
-for x in range(len(diffpos)):
-    s = remove_idx_from_string(s, diffpos.pop())
-
 # print results
 print(a1)
 print(a2)
 print(diffpos)
+
+s = a1
+for x in range(len(diffpos)):
+    s = remove_idx_from_string(s, diffpos.pop())
+
+
 print(s)
