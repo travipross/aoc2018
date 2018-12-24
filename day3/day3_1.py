a263d0e211e196b85ae9ba42578fb7ccd89fa504
travipross <travisprosser@gmail.com>
2018-12-24 13:48:26 -0400

    update to reuse for part 2

--- day3/day3_1.py
+++ day3/day3_1.py
@@ -6,7 +6,7 @@ with open("data3.txt") as f:
     data = f.read().strip()
     
 # parse data
-cuts = re.findall('#[0-9]+ @ ([0-9]+),([0-9]+): ([0-9]+)x([0-9]+)', data)
+cuts = re.findall('#([0-9]+) @ ([0-9]+),([0-9]+): ([0-9]+)x([0-9]+)', data)
 for idx, op in enumerate(cuts):
     cuts[idx] = list(map(lambda x: int(x), op))
 
@@ -15,10 +15,10 @@ arr = np.zeros([1000,1000], dtype=int)
 
 # add cuts
 def update_cuts(a, cut_list):
-    col_start = cut_list[0]
-    row_start = cut_list[1]
-    col_size = cut_list[2]
-    row_size = cut_list[3]
+    col_start = cut_list[1]
+    row_start = cut_list[2]
+    col_size = cut_list[3]
+    row_size = cut_list[4]
     col_end = col_start+col_size
     row_end = row_start+row_size
     
@@ -31,3 +31,5 @@ for c in cuts:
 # count overlapping areas (non-zero)
 num_overlap = (arr > 1).nonzero()[0].shape[0]
 print("Overlapping segments: %d" % num_overlap)
+
+#################### part 2 ########################
