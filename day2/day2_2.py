24cdd79c0381c1aa78c7969f54c6f70c11b5c630
travipross <travisprosser@gmail.com>
2018-12-24 09:06:19 -0400

    add solution for day2_2

---
+++ day2/day2_2.py
@@ -0,0 +1,37 @@
+from collections import Counter
+
+path = "data2.txt"
+
+with open(path) as f:
+    data = f.read().strip().splitlines()
+
+for idx in range(len(data)):
+    boxes = data.copy()
+    a1 = boxes.pop(idx)
+    for a2 in boxes:
+        # count different chars
+        diffct = 0
+        diffpos = []
+        for pos, char in enumerate(a2):
+            if a1[pos] != char:
+                diffct += 1
+                diffpos.append(pos)
+        if diffct == 1:
+            print("Found 1")
+            # if exactly 1 different char, check no more
+            break
+    if diffct == 1:
+        break
+
+def remove_idx_from_string(s, idx):
+    return s[:idx] + s[idx+1:]
+
+s = a1
+for x in range(len(diffpos)):
+    s = remove_idx_from_string(s, diffpos.pop())
+
+# print results
+print(a1)
+print(a2)
+print(diffpos)
+print(s)
