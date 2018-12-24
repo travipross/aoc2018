bb10441c1325912385e14eef23a197eb1cd314c0
travipross <travisprosser@gmail.com>
2018-12-24 00:20:57 -0400

    use itertools.cycle to loop and use a set instead of list

--- day1/day1_2.py
+++ day1/day1_2.py
@@ -1,9 +1,11 @@
-filename = 'data1_test1.txt'
+from itertools import cycle
+
+filename = 'data1.txt'
 
 with open(filename) as f:
     data = f.read().split('\n')
 
-list_of_totals = []
+list_of_totals = {0}
 total = 0
 l = len(data)
 idx = 0
@@ -17,24 +19,33 @@ def performStringOperation(tot, op):
         raise ValueError('No mathematical operator found on line: %s' % op)
     return tot
 
-# loop indefinitely and rely on break statement for exit
-while True:
-    # read and add/subtract data
-    total = performStringOperation(total, data[idx])
-    
-    # check exit condition
+# get list of totals for one full iteration 
+for x in cycle(data):
+    total = performStringOperation(total, x)
     if total in list_of_totals:
         break
-        
-    # append current frequency to list
-    list_of_totals.append(total)
-    
-    # loop around if at the end of the list of frequency shifts  
-    idx += 1
-    if idx >= l:
-        print("Loop reset.. %d items in list" % len(list_of_totals))
-        idx = 0
-    
-print("Frequency changes: %d" % len(list_of_totals))
+    list_of_totals.add(total)
+
+
+########## DUMB WAY TO DO IT ###########
+# # loop indefinitely and rely on break statement for exit
+# while True:
+#     # read and add/subtract data
+#     total = performStringOperation(total, data[idx])
+#     
+#     # check exit condition
+#     if total in list_of_totals:
+#         break
+#         
+#     # append current frequency to list
+#     list_of_totals.append(total)
+#     
+#     # loop around if at the end of the list of frequency shifts  
+#     idx += 1
+#     if idx >= l:
+#         print("Loop reset.. %d items in list" % len(list_of_totals))
+#         idx = 0
+#     
+# print("Frequency changes: %d" % len(list_of_totals))
 print("First repeating frequency: %s" % total)
 
