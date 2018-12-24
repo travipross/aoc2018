3339aaa675b60362daffffc621628a47f7516127
travipross <travisprosser@gmail.com>
2018-12-24 00:04:23 -0400

    modularize to reuse code in different attempt

--- day1/day1_2.py
+++ day1/day1_2.py
@@ -1,4 +1,4 @@
-filename = 'data1_test3.txt'
+filename = 'data1_test1.txt'
 
 with open(filename) as f:
     data = f.read().split('\n')
@@ -8,16 +8,19 @@ total = 0
 l = len(data)
 idx = 0
 
+def performStringOperation(tot, op):
+    if op.startswith('+'):
+        tot += int(op.split('+')[1])
+    elif op.startswith('-'):
+        tot -= int(op.split('-')[1])
+    else:
+        raise ValueError('No mathematical operator found on line: %s' % op)
+    return tot
+
 # loop indefinitely and rely on break statement for exit
 while True:
     # read and add/subtract data
-    x = data[idx]    
-    if x.startswith('+'):
-        total += int(x.split('+')[1])
-    elif x.startswith('-'):
-        total -= int(x.split('-')[1])
-    else:
-        raise ValueError('No mathematical operator found on line: %s' % x)
+    total = performStringOperation(total, data[idx])
     
     # check exit condition
     if total in list_of_totals:
