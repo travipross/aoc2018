e637301df75db045fd97d92818a9d08f448a4bb9
travipross <travisprosser@gmail.com>
2018-12-26 20:14:45 -0400

    update to properly fit question

--- day9/day9.py
+++ day9/day9.py
@@ -6,8 +6,8 @@ with open('data9.txt') as f:
 players = int(raw.split('players')[0])
 last_marble_val = int(raw.split('points')[0].split('worth')[1])
 
-players = 9
-last_marble_val = 32
+players = 10
+last_marble_val = 1618
 
 player_scores = defaultdict(int)
 last_marble_idx = 1
@@ -15,6 +15,7 @@ current_circle = [0, 1]
 marble_num = 2
 player_num = 2
 
+args = current_circle, last_marble_idx, marble_num, player_num
 
 def get_insertion_idx(circle, last_idx):
     if last_idx + 1 >= len(circle):
@@ -30,11 +31,12 @@ def get_pop_idx(circle, last_idx):
         return last_idx - 7
 
 
-def play_turn(current_circle, last_marble_idx, marble_num, player_num):
+def play_turn(current_circle, last_marble_idx, marble_num, player_num, last_score = 0):
     if marble_num % 23 == 0:
         # special case
         last_marble_idx = get_pop_idx(current_circle, last_marble_idx) # get new idx
-        player_scores[player_num] += marble_num + current_circle.pop(last_marble_idx)
+        last_score = marble_num + current_circle.pop(last_marble_idx)
+        player_scores[player_num] += last_score
     else:
         # regular case
         # get insertion index
@@ -43,9 +45,14 @@ def play_turn(current_circle, last_marble_idx, marble_num, player_num):
 
     marble_num += 1
     player_num = player_num + 1 if player_num < players else 1
-    return current_circle, last_marble_idx, marble_num, player_num
+    return current_circle, last_marble_idx, marble_num, player_num, last_score
 
 
-while marble_num <= 25:
-    print(player_num, current_circle)
-    current_circle, last_marble_idx, marble_num, player_num = play_turn(current_circle, last_marble_idx, marble_num, player_num)
+while True:
+    args = play_turn(*args)
+    if args[4] >= last_marble_val:
+        break
+
+winner = max(player_scores)
+score = player_scores[winner]
+print(args[4], winner, score)
